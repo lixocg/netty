d516bf6b23af3df769f1202b5acfd8104a24c89b
lixiongcheng <709597311@qq.com>
2020-03-29 16:46:03 +0800

    f

--- example/src/main/java/io/netty/example/mystudy/nio/NioFileChannel.java
+++ example/src/main/java/io/netty/example/mystudy/nio/NioFileChannel.java
@@ -9,11 +9,78 @@ import java.nio.charset.Charset;
 public class NioFileChannel {
     public static void main(String[] args) throws Exception {
         channelWrite();
-        channelRead();
+//        channelRead();
+//        channelCopy();
+//        trasferFromCopy();
+
+        transferToCopy();
+    }
+
+
+    public static void transferToCopy() throws Exception{
+        FileInputStream fileInputStream = new FileInputStream("/Users/lixiongcheng/a.txt");
+        FileOutputStream outputStream = new FileOutputStream("/Users/lixiongcheng/d.txt");
+
+        FileChannel srcChannel = fileInputStream.getChannel();
+        FileChannel destChannel = outputStream.getChannel();
+
+//        destChannel.transferFrom(srcChannel,0,srcChannel.size());
+        srcChannel.transferTo(0,srcChannel.size(),destChannel);
+
+        fileInputStream.close();
+        outputStream.close();
+
+        srcChannel.close();
+        destChannel.close();
+    }
+
+    public static void trasferFromCopy() throws Exception{
+        FileInputStream fileInputStream = new FileInputStream("/Users/lixiongcheng/a.txt");
+        FileOutputStream outputStream = new FileOutputStream("/Users/lixiongcheng/c.txt");
+
+        FileChannel srcChannel = fileInputStream.getChannel();
+        FileChannel destChannel = outputStream.getChannel();
+
+        destChannel.transferFrom(srcChannel,0,srcChannel.size());
+
+        fileInputStream.close();
+        outputStream.close();
+
+        srcChannel.close();
+        destChannel.close();
     }
 
     public static void channelCopy() throws Exception {
+        FileInputStream fileInputStream = new FileInputStream("/Users/lixiongcheng/a.txt");
+        FileChannel inputStreamChannel = fileInputStream.getChannel();
 
+        FileOutputStream outputStream = new FileOutputStream("/Users/lixiongcheng/b.txt");
+        FileChannel outputStreamChannel = outputStream.getChannel();
+
+        //读取数据到buffer中
+        ByteBuffer byteBuffer = ByteBuffer.allocate(5);
+
+        while (true){
+            //重要操作：需要复位一下,如果不复位：当分配的buffer小于属于长度时，第一次将读满buffer，
+            // 导致后续不能在读到数据，readBytes == 0，将会一直将第一次读到数据往outputStreamChannel中写
+            //clear() limit = position,position = 0,并不清空buffer数据
+            byteBuffer.clear();
+
+            int readBytes = inputStreamChannel.read(byteBuffer);
+
+            //数据读取完毕
+            if(readBytes == -1){
+                break;
+            }
+
+            //将buffer中数据写到outpuStreamChannel中
+            byteBuffer.flip();
+
+            outputStreamChannel.write(byteBuffer);
+        }
+
+        fileInputStream.close();
+        outputStream.close();
     }
 
     public static void channelRead() throws Exception {
@@ -37,7 +104,7 @@ public class NioFileChannel {
         ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
 
         //向Buffer中写入数据
-        byteBuffer.put("张三的歌".getBytes(Charset.defaultCharset()));
+        byteBuffer.put("张三的歌深V和是时候GV的深V的划分深V回复是和深V好地方".getBytes(Charset.defaultCharset()));
 
         //读写切换
         byteBuffer.flip();
